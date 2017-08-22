e4f7c34ba3f2bf97d8bfb561a29d6af808b52af4
hanniali <selective>
2017-08-22 17:37:41 -0300

    Initial commit

---
+++ iris.py
@@ -0,0 +1,14 @@
+%matplotlib inline
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn
+
+
+from sklearn.linear_model import LinearRegression
+from scipy import stats
+import pylab as pl
+
+seaborn.set()
+
+from IPython.display import Image
+Image("http://scikit-learn.org/dev/_static/ml_map.png", width=800)
